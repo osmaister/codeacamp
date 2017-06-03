10f21ce9a73200d807419a9aeef2b9caa705abe0
oswaldo nava <revenholic@hotmail.com>
2017-06-02 19:08:00 -0500

    segundo ejercicio primer semana

---
+++ prework/semana1/lunes_1/operadores.rb
@@ -0,0 +1,11 @@
+num1 = 15
+num2 = 6
+p num1.class
+p sum = num1 + num2
+p difference = num1 - num2
+p product = num1 * num2
+p quotient = num1 / num2.to_f
+p modulus = num1 % num2 
+
+
+p quotient.class
