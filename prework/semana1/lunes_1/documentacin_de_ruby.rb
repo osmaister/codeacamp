de85ad607dd179a92727c7fa1832f682dd0ddf84
oswaldo nava <revenholic@hotmail.com>
2017-06-05 20:38:28 -0500

    Semana1_Lunes_linea_de_comandos

---
+++ prework/semana1/lunes_1/documentacin_de_ruby.rb
@@ -0,0 +1,11 @@
+string = "oswaldo"
+
+p string.upcase
+p string.length
+p string.start_with? 'X'
+p string.end_with? 'Z'
+p string[string.length-1]
+p string[0]='z'
+p string
+p string.capitalize
+p string.chr
